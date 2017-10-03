28cee64faf537e825716a0f60b52d1dfbba5dccd
Dpat09 <dpat092@gmail.com>
2017-10-03 11:44:37 -0500

    Got nearest neighbor working. Used to fail when the size was too large. Also fixed the size of array that was created. Did not know fx,fy were strings, it would cause it to multiply the number to a very very large number.

--- resize/resample.py
+++ resize/resample.py
@@ -1,6 +1,6 @@
 class resample:
 
-    def resize(self, image, fx = None, fy = None, interpolation = None):
+    def resize(self, image, fx, fy, interpolation):
         """calls the appropriate funciton to resample an image based on the interpolation method
         image: the image to be resampled
         fx: scale along x direction (eg. 0.5, 1.5, 2.5)
@@ -22,14 +22,34 @@ class resample:
         fx: scale along y direction (eg. 0.5, 1.5, 2.5)
         returns a resized image based on the nearest neighbor interpolation method
         """
-
         #Write your code for nearest neighbor interpolation here
-        w, h = fx*len(image), fy*len(image)
+        w = image.shape[1]
+        h = image.shape[0]
 
+        newW = int(w*float(fx))
+        newH = int(h*float(fy))
 
+        ratioW = w/newW
+        ratioH = h/newH
 
+        import numpy as np
 
+        newImg = np.zeros((newH, newW), np.uint8)
+        for i in range(newImg.shape[0]):
+            for j in range(newImg.shape[1]):
 
+                x = round(ratioH*i)
+                y = round(ratioW*j)
+
+                if x == h:
+                    x-=1
+                if y == w:
+                    y-=1
+
+                temp = image[x, y]
+                newImg[i,j] = temp
+
+        image = newImg
         return image
 
 
